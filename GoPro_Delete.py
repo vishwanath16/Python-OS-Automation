e0b893c8c836e4c96b6da5c7f2c32061c0b00a93
VISHWANATH T S <31544005+vishwanath16@users.noreply.github.com>
2023-06-18 14:10:15 +0530

    Update GoPro_Delete.py

--- GoPro_Delete.py
+++ GoPro_Delete.py
@@ -1,7 +1,7 @@
 import os
 import shutil 
 
-path1 = "S:/GoPro Videos/Maramma/"
+path1 = "Path where you wanna make changes"
 
 for i in os.listdir(path1):
     if(i[-3:] == "THM" or i[-3:] =="LRV"):
